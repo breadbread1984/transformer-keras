03539110e7e93b27beff5d54499c9e6c5862f92c
Konquer your Mind <yi.xie@enflame-tech.com>
2023-12-06 08:41:49 +0000

    update

--- uniformer.py
+++ uniformer.py
@@ -3,12 +3,29 @@
 from os import environ
 environ['KERAS_BACKEND'] = 'tensorflow'
 import keras as K
+import keras_cv as KCV
 
 def CBlock(**kwargs):
     # args
-    in_channel = kwargs.get('in_channel', 768)
+    channel = kwargs.get('channel', 768)
+    drop_path_rate = kwargs.get('drop_path_rate', 0.)
     # network
-    inputs = K.Input((None, None, None, in_channel))
+    inputs = K.Input((None, None, None, channel))
+    skip = inputs
+    pos_embed = K.layers.Conv3D(channel, kernel_size = (3,3,3), padding = 'same')(inputs)
+    results = K.layers.Add()([skip, pos_embed])
+    skip = results
+    results = K.layers.BatchNormalization()(results)
+    results = K.layers.Conv3D(channel, kernel_size = (1,1,1), padding = 'same')(results)
+    results = K.layers.Conv3D(channel, kernel_size = (5,5,5), strides = (2,2,2), padding = 'same')(results)
+    results = K.layers.Conv3D(channel, kernel_size = (1,1,1), padding = 'same')(results)
+    if drop_path_rate > 0:
+        results = KCV.layers.DropPath(rate = drop_path_rate)(results)
+    else:
+        results = K.layers.Identity()(results)
+    results = K.layers.Add()([skip, results])
+    skip = results
+    results = K.layers.BatchNormalization()(results)
     
 
 def Uniformer(**kwargs):
@@ -19,6 +36,6 @@ def Uniformer(**kwargs):
     # network
     inputs = K.Input((None, None, None, in_channel)) # inputs.shape = (batch, t, h, w, in_channel)
     results = K.layers.Conv3D(out_channel, kernel_size = (patch_size, patch_size, patch_size), strides = (patch_size, patch_size, patch_size), padding = 'same')(inputs) # results.shape = (batch, t / patch_size, h / patch_size, w / patch_size, out_channel)
-    results = K.layers.LayerNorm()(results)
+    results = K.layers.LayerNormalization()(results)
     results = K.layers.Dropout(rate = drop_rate)(results)
     
