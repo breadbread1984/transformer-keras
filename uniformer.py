4b0f2491ced7a155317c904f2902761b5a74d651
Konquer your Mind <yi.xie@enflame-tech.com>
2023-12-06 08:51:41 +0000

    update

--- uniformer.py
+++ uniformer.py
@@ -9,6 +9,8 @@ def CBlock(**kwargs):
     # args
     channel = kwargs.get('channel', 768)
     drop_path_rate = kwargs.get('drop_path_rate', 0.)
+    mlp_ratio = kwargs.get('mlp_ratio', 4)
+    drop_rate = kwargs.get('drop_rate', 0.)
     # network
     inputs = K.Input((None, None, None, channel))
     skip = inputs
@@ -26,7 +28,15 @@ def CBlock(**kwargs):
     results = K.layers.Add()([skip, results])
     skip = results
     results = K.layers.BatchNormalization()(results)
-    
+    results = K.layers.Conv3D(channel, kernel_size = (1,1,1), padding = 'same', activation = K.activations.gelu)(results)
+    results = K.layers.Conv3D(channel * mlp_ratio, kernel_size = (1,1,1), padding = 'same')(results)
+    results = K.layers.Dropout(rate = drop_rate)(results)
+    if drop_path_rate > 0:
+        results = KCV.layers.DropPath(rate = drop_path_rate)(results)
+    else:
+        results = K.layers.Identity()(results)
+    results = K.layers.Add()([skip, results])
+    return K.Model(inputs = inputs, outputs = results)
 
 def Uniformer(**kwargs):
     # args
