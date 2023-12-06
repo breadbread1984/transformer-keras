d790fc8374c18bb074ca90a844aeb601f02364f9
Konquer your Mind <yi.xie@enflame-tech.com>
2023-12-06 09:08:18 +0000

    update

--- uniformer.py
+++ uniformer.py
@@ -2,6 +2,7 @@
 
 from os import environ
 environ['KERAS_BACKEND'] = 'tensorflow'
+import numpy as np
 import keras as K
 import keras_cv as KCV
 
@@ -16,6 +17,7 @@ def CBlock(**kwargs):
     skip = inputs
     pos_embed = K.layers.Conv3D(channel, kernel_size = (3,3,3), padding = 'same')(inputs)
     results = K.layers.Add()([skip, pos_embed])
+
     skip = results
     results = K.layers.BatchNormalization()(results)
     results = K.layers.Conv3D(channel, kernel_size = (1,1,1), padding = 'same')(results)
@@ -26,6 +28,7 @@ def CBlock(**kwargs):
     else:
         results = K.layers.Identity()(results)
     results = K.layers.Add()([skip, results])
+
     skip = results
     results = K.layers.BatchNormalization()(results)
     results = K.layers.Conv3D(channel, kernel_size = (1,1,1), padding = 'same', activation = K.activations.gelu)(results)
@@ -41,11 +44,18 @@ def CBlock(**kwargs):
 def Uniformer(**kwargs):
     # args
     in_channel = kwargs.get('in_channel', 3)
-    out_channel = kwargs.get('out_channel', 768)
+    hidden_channels = kwargs.get('hidden_channels', [64,128,320,512])
     drop_rate = kwargs.get('drop_rate', 0.3)
+    global_drop_path_rate = kwargs.get('global_drop_path_rate', 0.)
+    depth = kwargs.get('depth', [5,8,20,7])
+    assert len(hidden_channels) == len(depth)
+    mlp_ratio = kwargs.get('mlp_ratio', 4.)
+    dpr = [x.item() for x in np.linspace(0, global_drop_path_rate, sum(depth))]
     # network
     inputs = K.Input((None, None, None, in_channel)) # inputs.shape = (batch, t, h, w, in_channel)
-    results = K.layers.Conv3D(out_channel, kernel_size = (patch_size, patch_size, patch_size), strides = (patch_size, patch_size, patch_size), padding = 'same')(inputs) # results.shape = (batch, t / patch_size, h / patch_size, w / patch_size, out_channel)
+    results = K.layers.Conv3D(hidden_channels[0], kernel_size = (patch_size, patch_size, patch_size), strides = (patch_size, patch_size, patch_size), padding = 'same')(inputs) # results.shape = (batch, t / patch_size, h / patch_size, w / patch_size, out_channel)
     results = K.layers.LayerNormalization()(results)
     results = K.layers.Dropout(rate = drop_rate)(results)
+    for i in range(depth[0]):
+        results = CBlock(channel = results.shape[-1], drop_path_rate = dpr[i], **kwargs)(results)
     
