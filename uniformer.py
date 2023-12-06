de6eda3f99fc163b651bb9ecf9c1288b67934425
Konquer your Mind <yi.xie@enflame-tech.com>
2023-12-06 09:30:17 +0000

    update

--- uniformer.py
+++ uniformer.py
@@ -53,9 +53,17 @@ def Uniformer(**kwargs):
     dpr = [x.item() for x in np.linspace(0, global_drop_path_rate, sum(depth))]
     # network
     inputs = K.Input((None, None, None, in_channel)) # inputs.shape = (batch, t, h, w, in_channel)
-    results = K.layers.Conv3D(hidden_channels[0], kernel_size = (patch_size, patch_size, patch_size), strides = (patch_size, patch_size, patch_size), padding = 'same')(inputs) # results.shape = (batch, t / patch_size, h / patch_size, w / patch_size, out_channel)
+    results = K.layers.Conv3D(hidden_channels[0], kernel_size = (4, 4, 4), strides = (4, 4, 4), padding = 'same')(inputs) # results.shape = (batch, t / 4, h / 4, w / 4, hidden_channels[0])
     results = K.layers.LayerNormalization()(results)
     results = K.layers.Dropout(rate = drop_rate)(results)
+    # block 1
     for i in range(depth[0]):
         results = CBlock(channel = hidden_channel[0], drop_path_rate = dpr[i], **kwargs)(results)
-    
+    results = K.layers.Conv3D(hidden_channels[0], kernel_size = (2, 2, 2), strides = (2, 2, 2), padding = 'same')(results) # results.shape = (batch, t / 8, h / 8, w / 8, hidden_channels[0])
+    results = K.layers.LayerNormalization()(results)
+    # block 2
+    for i in range(depth[1]):
+        results = CBlock(channel = hidden_channel[1], drop_path_rate = dpr[i], **kwargs)(results)
+    results = K.layers.Conv3D(hidden_channels[1], kernel_size = (2, 2, 2), strides = (2, 2, 2), padding = 'same')(results) # results.shape = (batch, t / 16, h / 16, w / 16, hidden_channels[1])
+    results = K.layers.LayerNormalization()(results)
+    # block 3
